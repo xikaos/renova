f2a27f317ab7347c92ff7cc5f7f1453a42d3b715
Francisco Zanatta - 'Xikaos <francisco@organicadigital.com>
2016-04-05 20:57:29 -0300

    User mgmt

---
+++ main.rb
@@ -0,0 +1,16 @@
+require "json"
+
+class Secret
+  attr_accessor :name, :password
+
+  def initialize name, pass
+    @name = name
+    @password = pass
+  end
+
+  def json
+    {name: @name, password: @password}.to_json
+  end
+
+
+end
